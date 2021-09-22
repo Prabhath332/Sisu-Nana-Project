245a9ae4f49d0b9779595a1165678fe7119cfc79
ruchikamperera <ruchikamperera@outlook.com>
2021-09-22 19:27:20 +0530

    login and create user

--- Data/web_projectContext.cs
+++ Data/web_projectContext.cs
@@ -61,10 +61,7 @@ namespace web_project.Data
             builder.ApplyConfiguration(new UserSeed());
             builder.ApplyConfiguration(new RoleSeed());
             builder.ApplyConfiguration(new UserRoleSeed());
-          
-
-
-
+         
 
         }
 
@@ -85,120 +82,17 @@ namespace web_project.Data
             {
                 Id = "72e3e2bd-b94a-4996-b684-07b9d88f9841",
                 UserName = "Admin",
-                Email = "ruchikamperera@outlook.com",
-                NormalizedEmail = "ruchikamperera@outlook.com",
+                Email = "admin@outlook.com",
+                NormalizedEmail = "admin@outlook.com",
                 NormalizedUserName = "Admin",
 
             };
-            IdentityUser user1 = new IdentityUser()
-            {
-                Id = "11e3e2bd-b94a-4996-b684-07b9d88f9841",
-                UserName = "Admin",
-                Email = "senath@outlook.com",
-                NormalizedEmail = "ruchikamperera@outlook.com",
-                NormalizedUserName = "Senath_Admin",
-
-            };
-
-            IdentityUser user2 = new IdentityUser()
-            {
-                Id = "11e3e2cd-b94a-4996-b684-17b9d88f9841",
-                UserName = "TestAdmin",
-                Email = "TestAdmin@outlook.com",
-                NormalizedEmail = "ruchikamperera@outlook.com",
-                NormalizedUserName = "Senath_Admin",
-
-            };
-            IdentityUser Nadeesha = new IdentityUser()
-            {
-                Id = "11e3e2cd-b94a-4996-b684-17bbd88f9861",
-                UserName = "Nadeesha",
-                Email = "Nadeesha@outlook.com",
-                NormalizedEmail = "Nadeesha@outlook.com",
-                NormalizedUserName = "Nadeesha",
-               
-
-            };
-            Nadeesha.PasswordHash = passwordHasher.HashPassword(Nadeesha, "Nadeesha@199328");
-
-            IdentityUser Mekala = new IdentityUser()
-            {
-                Id = "19e3e2cc-b94a-4996-b684-17bbd78f9861",
-                UserName = "Mekala",
-                Email = "Mekala@outlook.com",
-                NormalizedEmail = "Mekala@outlook.com",
-                NormalizedUserName = "Mekala",
-
-
-            };
-            Mekala.PasswordHash = passwordHasher.HashPassword(Mekala, "Mekala&1989");
-
-            IdentityUser Dinali = new IdentityUser()
-            {
-                Id = "14e3e2dc-b94a-4996-l685-17bbz78f98x1",
-                UserName = "Dinali",
-                Email = "Dinali@outlook.com",
-                NormalizedEmail = "Dinali@outlook.com",
-                NormalizedUserName = "Dinali",
-
-
-            };
-            Dinali.PasswordHash = passwordHasher.HashPassword(Dinali, "Dinali@0104");
-
-            IdentityUser MADUKA = new IdentityUser()
-            {
-                Id = "19ede2cc-b94a-4996-b684-17b5d78f986c",
-                UserName = "MADUKA",
-                Email = "MADUKA@outlook.com",
-                NormalizedEmail = "MADUKA@outlook.com",
-                NormalizedUserName = "MADUKA",
-
-
-            };
-            MADUKA.PasswordHash = passwordHasher.HashPassword(MADUKA, "MADUKA@123");
-
-            IdentityUser Kosala = new IdentityUser()
-            {
-                Id = "19e3e2cc-b94a-1196-b684-17bbd78f9861",
-                UserName = "Kosala",
-                Email = "Kosala@outlook.com",
-                NormalizedEmail = "Kosala@outlook.com",
-                NormalizedUserName = "Kosala",
-
-
-            };
             
-            Kosala.PasswordHash = passwordHasher.HashPassword(Kosala, "Kosala@1234");
-
-            IdentityUser Gamage = new IdentityUser()
-            {
-                Id = "99ede2cc-b94a-4996-b684-17b5d78f986c",
-                UserName = "Gamage",
-                Email = "Gamage@outlook.com",
-                NormalizedEmail = "Gamage@outlook.com",
-                NormalizedUserName = "Gamage",
-
-
-            };
-            Gamage.PasswordHash = passwordHasher.HashPassword(Gamage, "Gamage@1234");
-
-
-            user.PasswordHash = passwordHasher.HashPassword(user, "Password@1996");
-            user1.PasswordHash = passwordHasher.HashPassword(user1, "Password@12345");
-            user2.PasswordHash = passwordHasher.HashPassword(user2, "TestAdmin@123");
-
-
-
+            user.PasswordHash = passwordHasher.HashPassword(user, "Password@123");
+           
             builder.HasData
             (
-                user,
-                user1,
-                Nadeesha,
-                Mekala,
-                Dinali,
-                MADUKA,
-                Kosala,
-                Gamage
+                user               
             );
         }
     }
